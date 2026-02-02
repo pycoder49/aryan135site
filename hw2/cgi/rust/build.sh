674fc56b3c4aab502cab39841da6935446d1b9d1
pycoder49 <aryan-a@outlook.com>
2026-02-01 17:46:22 -0800

    try build.sh again

--- hw2/cgi/rust/build.sh
+++ hw2/cgi/rust/build.sh
@@ -3,9 +3,9 @@ cd "$(dirname "$0")"
 set -e
 
 rustc hello-html-rust.rs -O -o hello-html-rust.cgi
-rustc hello-json-rust.rs -O -o hello-json-rust.cgi
-rustc environment-rust.rs -O -o environment-rust.cgi
-rustc echo-rust.rs -O -o echo-rust.cgi
-rustc state-rust.rs -O -o state-rust.cgi
+# rustc hello-json-rust.rs -O -o hello-json-rust.cgi
+# rustc environment-rust.rs -O -o environment-rust.cgi
+# rustc echo-rust.rs -O -o echo-rust.cgi
+# rustc state-rust.rs -O -o state-rust.cgi
 
 chmod 755 *.cgi
