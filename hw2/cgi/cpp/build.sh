4057e3294ae0fd766d6a1e002d05c850e3863760
pycoder49 <aryan-a@outlook.com>
2026-02-01 20:47:00 -0800

    trying again

--- hw2/cgi/cpp/build.sh
+++ hw2/cgi/cpp/build.sh
@@ -3,8 +3,8 @@ cd "$(dirname "$0")"
 set -e
 
 g++ -O2 -std=c++17 hello-html-cpp.cpp -o hello-html-cpp.cgi
-g++ -O2 -std=c++17 hello-json-cpp.cpp -o hello-json-cpp.cgi
-g++ -O2 -std=c++17 environment-cpp.cpp -o environment-cpp.cgi
-g++ -O2 -std=c++17 echo-cpp.cpp -o echo-cpp.cgi
-g++ -O2 -std=c++17 state-cpp.cpp -o state-cpp.cgi
+# g++ -O2 -std=c++17 hello-json-cpp.cpp -o hello-json-cpp.cgi
+# g++ -O2 -std=c++17 environment-cpp.cpp -o environment-cpp.cgi
+# g++ -O2 -std=c++17 echo-cpp.cpp -o echo-cpp.cgi
+# g++ -O2 -std=c++17 state-cpp.cpp -o state-cpp.cgi
 chmod 755 *.cgi
